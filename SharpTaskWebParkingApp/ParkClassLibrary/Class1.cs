144eb2417603eb52fca60ec3a1b19785d94a80e2
HitoDZ <hitodz97@gmail.com>
2018-05-14 19:05:43 +0300

    InitCommit

---
+++ SharpTaskWebParkingApp/ParkClassLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ParkClassLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ SharpTaskWebParkingApp/SharpTaskWebParkingApp/Controllers/CarsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SharpTaskWebParkingApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+
+    public class CarsController : Controller
+    {
+        // GET: api/Cars
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value22" };
+        }
+
+        // GET: api/Cars/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Cars
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
