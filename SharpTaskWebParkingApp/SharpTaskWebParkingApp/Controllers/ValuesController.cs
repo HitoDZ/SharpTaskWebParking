dfcb07f61f09235fe176ce279553f366f5865884
HitoDZ <hitodz97@gmail.com>
2018-05-15 17:41:26 +0300

    Final of Final commit

--- SharpTaskWebParkingApp/SharpTaskWebParkingApp/Controllers/ValuesController.cs
+++ SharpTaskWebParkingApp/SharpTaskWebParkingApp/Controllers/ValuesController.cs
@@ -23,11 +23,12 @@ namespace SharpTaskWebParkingApp.Controllers
                  "<body><b>Welcome</b> " +
                 "</br></br></br><hr><a href='Transactions'>api/Transactions - Transaction.log </a>" +
                 "</br><a href='Transactions/GetMinTransactions'>api/Transactions/GetMinTransactions - transactions (1 min) </a>" +
+                "</br><a> PUT: api/Transactions/[id] -(int id, [FromBody]double value) - set id and money wich you add to cur balance</a>" +
                 "</br><a >api/Transactions/GetMinTransactions/[id] - transaction of cur car (1 min) </a><hr>" +
 
-                "</br><hr><a href='Parking/occupiedplaces'>api/Transactions/occupiedplaces - Free places </a>" +
-                "</br><a href='Parking/freeplaces'>api/Transactions/freeplaces - num of occupied places </a>" +
-                "</br><a href='Parking/balance'>api/Transactions/balance - balance of Parking </a><hr>" +
+                "</br><hr><a href='Parking/occupiedplaces'>api/Parking/occupiedplaces - Free places </a>" +
+                "</br><a href='Parking/freeplaces'>api/Parking/freeplaces - num of occupied places </a>" +
+                "</br><a href='Parking/balance'>api/Parking/balance - balance of Parking </a><hr>" +
 
                 "</br><hr><a href='Cars'>api/Cars - all Cars </a>" +
                 "</br><a > POST: api/Cars ([FromBody]double value) - Set Balance of car. (Add only TRACKS cause project without UI)</a>" +
@@ -38,30 +39,6 @@ namespace SharpTaskWebParkingApp.Controllers
                 "</html>"
             };
         }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+   
     }
 }
